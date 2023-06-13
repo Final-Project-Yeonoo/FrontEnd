f8a6dcb399657970efdab72ec0f3a4db478ba8d1
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-06-13 21:47:06 +0900

    setting commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
+// import '../src/components/login/login';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+
   );
 }
 
