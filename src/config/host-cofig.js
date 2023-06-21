a3af4f2c7ce7d8580919c76de5f0322c8391a889
WONI2 <126743165+WONI2@users.noreply.github.com>
2023-06-21 12:02:19 +0900

    userList css commit + config

--- src/config/host-cofig.js
+++ src/config/host-cofig.js
@@ -4,7 +4,7 @@ const clientHostName = window.location.hostname;
 let backEndHostName; //백엔드 서버 호스트 이름
 
 // if(clientHostName === 'localhost') {
-//     backEndHostName = 'http://localhost:/';
+//     backEndHostName = 'http://localhost:8888/';
 // }else if(clientHostName === '.com'){
 //     backEndHostName = 'http://';
 // }
@@ -16,3 +16,12 @@ export const company ='api/ynfinal/comp';
 export const trading ='api/ynfinal/trcomp';
 export const mypage ='api/ynfinal/mypage';
 export const department ='api/ynfinal/department';
+export const adminpage ='api/ynfinal/admin';
+export const raw ='api/ynfinal/rawitem';
+export const half ='api/ynfinal/halfitem';
+export const finished ='api/ynfinal/finisheditem';
+export const store ='api/ynfinal/store';
+export const purchase ='api/ynfinal/itemorder';
+export const imports ='api/ynfinal/inputproduct';
+export const inventory ='api/ynfinal/stock';
+export const orderdetail ='api/ynfinal/orderdetail';
