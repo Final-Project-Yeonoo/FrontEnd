26d484ef812944230af14f9cfe1325c8512b336b
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-06-25 02:15:25 +0900

    로그인 프론트 인증처리

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -3,12 +3,22 @@ import {Container, Form, Button} from 'react-bootstrap';
 import styles from './css/Login.module.css';
 
 const Login = () => {
+    const [companyCode, setCompanyCode] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // 여기에 로그인 처리 로직을 추가하세요
+
+        // 폼 유효성 검사를 수행하고 필요한 경우 경고 메시지를 표시합니다.
+        if (!companyCode || !email || !password) {
+            alert('모든 값을 입력해주세요.');
+            return;
+        }
+
+        // 로그인 처리 로직을 추가하세요
+        // API 호출이나 다른 필요한 로직을 수행할 수 있습니다.
+        console.log('로그인 처리 로직 실행');
     };
 
     return (
@@ -19,14 +29,14 @@ const Login = () => {
                         <div className= {styles.imgContainer} >
                             <img src="/logo.png" alt="ERP시스템 로고"  className={styles.imgSize}/>
                         </div>
-                        <Form.Group controlId="formBasicEmail">
+                        <Form.Group controlId="formBasicCompanyCode">
                             <Form.Label></Form.Label>
                             <Form.Control
-                                style={{marginLeft:'0'}}
+                                style={{ marginLeft: '0' }}
                                 type="text"
                                 placeholder="Company Code"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
+                                value={companyCode}
+                                onChange={(e) => setCompanyCode(e.target.value)}
                             />
                         </Form.Group>
                         <Form.Group controlId="formBasicEmail">
