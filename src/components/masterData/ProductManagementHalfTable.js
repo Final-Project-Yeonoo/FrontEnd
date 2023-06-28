38043dc93e3221ba082d20399b1a9b4cf832dba3
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-06-28 19:25:08 +0900

    원자재, 반자재, 완자재 crud 수정했으나 일부 안됨

--- src/components/masterData/ProductManagementHalfTable.js
+++ src/components/masterData/ProductManagementHalfTable.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import React, {useEffect, useState} from 'react';
+import {DataGrid} from '@mui/x-data-grid';
 import axios from "axios";
 import styles from "./css/ProductManagement.module.css";
 import Form from "react-bootstrap/Form";
@@ -8,11 +8,9 @@ import Col from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 
-
 function OrangeInputforHalf() {
 
     const [inputValue, setInputValue] = useState([]);
-
     const handleInputChange = (index, value) => {
         const newInputValue = [...inputValue];
         newInputValue[index] = value;
@@ -29,15 +27,13 @@ function OrangeInputforHalf() {
 
     // 저장 버튼 선택시
     const handleSubmit = () => {
-
         const data = {
             // "halfCode": inputValue[0],
             "halfName": inputValue[0],
             "halfComment": inputValue[1]
         }
 
-
-        console.log(data);
+        // console.log(data);
 
         axios
             .post('http://localhost:8888/ynfinal/halfitem', data)
@@ -47,7 +43,7 @@ function OrangeInputforHalf() {
             .catch(error => {
                 console.error('실패함', error);
             });
-    }
+    };
 
 
     return (
@@ -65,8 +61,8 @@ function OrangeInputforHalf() {
                                 <Form.Control className="mb-2"
                                               id="inlineFormInput"
                                               placeholder='자동 완성'
-                                              // value={inputValue[0]}
-                                              // onChange={(e) => handleInputChange(0, e.target.value)}
+                                    // value={inputValue[0]}
+                                    // onChange={(e) => handleInputChange(0, e.target.value)}
                                               readOnly
                                 />
                             </Col>
@@ -75,15 +71,18 @@ function OrangeInputforHalf() {
                     <Row>
                         <div style={{display: 'flex'}}>
                             <Col xs="auto">
-                                <Form.Control readOnly placeholder='반제품명'
-                                              style={{marginRight: '10px', width: '150px'}}/>
+                                <Form.Control
+                                    readOnly
+                                    placeholder='반제품명'
+                                    style={{marginRight: '10px', width: '150px'}}/>
                             </Col>
                             <Col xs="auto">
-                                <Form.Control className="mb-2"
-                                              id="inlineFormInput"
-                                              placeholder='입력하세요'
-                                              value={inputValue[0]}
-                                              onChange={(e) => handleInputChange(0, e.target.value)}
+                                <Form.Control
+                                    className="mb-2"
+                                    id="inlineFormInput"
+                                    placeholder='입력하세요'
+                                    value={inputValue[0]}
+                                    onChange={(e) => handleInputChange(0, e.target.value)}
                                 />
                             </Col>
                         </div>
@@ -91,7 +90,8 @@ function OrangeInputforHalf() {
                     <Row>
                         <div style={{display: 'flex'}}>
                             <Col xs="auto">
-                                <Form.Control readOnly placeholder='비고'
+                                <Form.Control readOnly
+                                              placeholder='비고'
                                               style={{marginRight: '10px', width: '150px'}}/>
                             </Col>
                             <Col xs="auto">
@@ -107,11 +107,7 @@ function OrangeInputforHalf() {
                 </Form>
             </div>
             <div className={styles.navRight}>
-                <Button variant="primary" onClick={() => {
-                }}>조회</Button>
                 <Button variant="success" onClick={handleSubmit}>저장</Button>
-                <Button variant="danger" onClick={() => {
-                }}>삭제</Button>
                 <Button variant="secondary" onClick={handleReset}>초기화</Button>
             </div>
         </>
@@ -123,56 +119,110 @@ function OrangeInputforHalf() {
 
 const ProductManagementHalfTable = () => {
     const CustomPagination = () => null;
-
     const [rows, setRows] = useState([]);
-    const [selectedRows, setSelectedRows] = useState([]);
-
-    const fetchGridData = async () => {
-        try {
-            const response = await fetch('http://localhost:8888/ynfinal/halfitem');
-            const data = await response.json();
-
-            // Generate unique IDs for the rows based on their index
-            const rowsWithIds = data.map((row, index) => ({ ...row, id: index + 1 }));
-
-            setRows(rowsWithIds);
-        } catch (error) {
-            console.error('Error fetching grid data:', error);
-        }
-    };
 
     useEffect(() => {
+
+        const fetchGridData = async () => {
+            try {
+                const response = await fetch('http://localhost:8888/ynfinal/halfitem');
+                const data = await response.json();
+
+                // Generate unique IDs for the rows based on their index
+                const rowsWithIds = data.map((row, index) => ({...row, id: index + 1}));
+
+                setRows(rowsWithIds);
+            } catch (error) {
+                console.error('Error fetching grid data:', error);
+            }
+        };
+
         fetchGridData();
     }, []);
 
-    const columns = [
-        { field: 'halfCode', headerName: '반제품 코드', width: 150 },
-        { field: 'halfName', headerName: '반제품명', width: 150 },
-        { field: 'halfCount', headerName: '수량', width: 150, editable: true },
-        { field: 'halfPrice', headerName: '금액', width: 150, editable: true },
-        { field: 'storehouseCode', headerName: '창고 번호', width: 150, editable: true },
-        { field: 'empNo', headerName: '사원 번호', width: 150 },
-        { field: 'halfRegDate', headerName: ' 반제품 등록일', width: 150 },
-        { field: 'halfRegUpdate', headerName: ' 반제품 수정일', width: 150 },
-        { field: 'halfComment', headerName: '비고', width: 150 },
-    ];
 
-    const handleSelectionModelChange = (selection) => {
-        setSelectedRows(selection);
-        console.log(selection); // 선택한 행 출력
+    // 행 선택
+    const [selectedRow, setSelectedRow] = useState(null);
+    const handleRowClick = (params) => {
+
+        const selectedRowData = params.row;
+
+        console.log("선택된 row의 정보:", selectedRowData);
+
+        setSelectedRow(selectedRowData);
     };
 
-    return (
-        <div style={{ height: 800, width: '100%' }}>
-            <DataGrid rows={rows} columns={columns} getRowId={(row) => row.id}
-                      components={{
-                          Pagination: CustomPagination,
-                      }}
-                      checkboxSelection={true}
-                      selectionModel={selectedRows}
-                      onSelectionModelChange={handleSelectionModelChange}
+    // 수정 버튼
+    const handleModifyClick = async () => {
+        const arrayData = selectedRow
+        try {
+            const response = await fetch('http://localhost:8888/ynfinal/halfitem', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(arrayData),
+            });
+            console.log('선택정보 수정확인', arrayData);
 
-            />
+            if (!response.ok) {
+                throw new Error('Failed to save data');
+            }
+            console.log('Data saved successfully');
+        } catch (error) {
+            console.error('Error saving data:', error);
+        }
+    };
+
+    // 삭제 버튼
+    const handleDeleteClick = async () => {
+        const arrayData = selectedRow.halfCode
+        try {
+            const response = await fetch('http://localhost:8888/ynfinal/halfitem/' + arrayData, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(arrayData),
+            });
+            console.log('선택정보 삭제확인', arrayData);
+
+            if (!response.ok) {
+                throw new Error('Failed to save data');
+            }
+
+            console.log('Data saved successfully');
+        } catch (error) {
+            console.error('Error saving data:', error);
+        }
+    };
+
+
+    return (
+        <div style={{height: 800, width: '100%'}}>
+            <div style={{marginBottom: '1rem'}}>
+                <div style={{marginBottom: '20px'}}>
+                    <Button variant="primary" onClick={handleModifyClick} style={{marginRight: '10px'}}>수정 저장</Button>
+                    <Button variant="danger" onClick={handleDeleteClick}>삭제</Button>
+                </div>
+                <DataGrid
+                    rows={rows}
+                    columns={[
+                        {field: 'halfCode', headerName: '반제품 코드', width: 150},
+                        {field: 'halfName', headerName: '반제품명', width: 150},
+                        {field: 'halfCount', headerName: '수량', width: 150, editable: true},
+                        {field: 'halfPrice', headerName: '금액', width: 150, editable: true},
+                        // {field: 'storehouseCode', headerName: '창고 번호', width: 150, editable: true},
+                        {field: 'empNo', headerName: '사원 번호', width: 150},
+                        {field: 'halfRegDate', headerName: ' 반제품 등록일', width: 150},
+                        {field: 'halfRegUpdate', headerName: ' 반제품 수정일', width: 150},
+                        {field: 'halfComment', headerName: '비고', width: 150},
+                    ]}
+                    checkboxSelection={true}
+                    onRowClick={handleRowClick}
+                    getRowId={(row) => row.id}
+                />
+            </div>
         </div>
     );
 };
--- src/components/masterData/ProductManagementRawTable.js
+++ src/components/masterData/ProductManagementRawTable.js
@@ -5,9 +5,7 @@ import Form from "react-bootstrap/Form";
 import {Row} from "reactstrap";
 import Col from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-// import './css/ProductManagementRawTable.css'
 import styles from "./css/ProductManagement.module.css";
-import {Checkbox} from "@mui/material";
 
 function OrangeInputforRaw() {
 
@@ -18,7 +16,7 @@ function OrangeInputforRaw() {
         setInputValue(newInputValue);
     };
 
-    console.log(inputValue)
+    // console.log(inputValue)
 
     // 초기화  버튼 선택시
     const handleReset = () => {
@@ -35,14 +33,13 @@ function OrangeInputforRaw() {
                 rawType: inputValue[1]
             };
 
-            console.log(data);
-
+            // console.log(data);
             axios
                 .post('http://localhost:8888/ynfinal/rawitem', data)
                 .then(response => {
-                    const { rawCode } = response.data;
+                    // const {rawCode} = response.data;
                     console.log(response.data);
-                    console.log('Received rawCode:', rawCode); // rawCode를 콘솔에 출력
+
                 })
                 .catch(error => {
                     console.error('실패함', error);
@@ -125,11 +122,12 @@ function OrangeInputforRaw() {
 
 }
 
+
 const ProductManagementRawTable = () => {
     const CustomPagination = () => null;
     const [rows, setRows] = useState([]);
 
-    // 저장 시 정보 전달
+    // 저장 시 정보 전달 : 페이지 클릭시 백에서 정보 한 번만 나오게 적용
     useEffect(() => {
         const fetchGridData = async () => {
             try {
@@ -144,17 +142,18 @@ const ProductManagementRawTable = () => {
         fetchGridData();
     }, []);
 
-    // 행 선탥
+    // 행 선택
     const [selectedRow, setSelectedRow] = useState(null);
     const handleRowClick = (params) => {
 
         const selectedRowData = params.row;
 
         console.log("선택된 row의 정보:", selectedRowData);
+
         setSelectedRow(selectedRowData);
     };
 
-    // 수정 버튼 클릭시 백에 전달
+    // 수정 버튼
     const handleModifyClick = async () => {
         const arrayData = selectedRow
         try {
@@ -165,30 +164,29 @@ const ProductManagementRawTable = () => {
                 },
                 body: JSON.stringify(arrayData),
             });
-            console.log('선택정보 수정확인',arrayData);
+            console.log('선택정보 수정확인', arrayData);
 
             if (!response.ok) {
                 throw new Error('Failed to save data');
             }
-
             console.log('Data saved successfully');
         } catch (error) {
             console.error('Error saving data:', error);
         }
     };
 
-    // 수정 버튼 클릭시 백에 전달
+    // 삭제
     const handleDeleteClick = async () => {
         const arrayData = selectedRow.rawCode
         try {
-            const response = await fetch('http://localhost:8888/ynfinal/rawitem/'+`${arrayData}` , {
+            const response = await fetch('http://localhost:8888/ynfinal/rawitem/' + `${arrayData}`, {
                 method: 'DELETE',
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(arrayData),
             });
-            console.log('선택정보 삭제확인',arrayData);
+            console.log('선택정보 삭제확인', arrayData);
 
             if (!response.ok) {
                 throw new Error('Failed to save data');
@@ -203,8 +201,8 @@ const ProductManagementRawTable = () => {
     return (
         <div style={{height: 800, width: '100%'}}>
             <div style={{marginBottom: '1rem'}}>
-                <div style={{marginBottom:'20px'}}>
-                    <Button variant="primary" sytle={{marginRight:'10px'}} onClick={handleModifyClick}>수정 완료</Button>
+                <div style={{marginBottom: '20px'}}>
+                    <Button variant="primary" onClick={handleModifyClick} style={{marginRight:'10px'}}>수정 저장</Button>
                     <Button variant="danger" onClick={handleDeleteClick}>삭제</Button>
                 </div>
                 <DataGrid
@@ -215,7 +213,7 @@ const ProductManagementRawTable = () => {
                         {field: 'rawType', headerName: '유형', width: 150},
                         {field: 'rawCount', headerName: '수량', width: 150, editable: true},
                         {field: 'rawPrice', headerName: '금액', width: 150, editable: true},
-                        {field: 'storehouseCode', headerName: '창고 번호', width: 150, editable: true},
+                        // {field: 'storehouseCode', headerName: '창고 번호', width: 150, editable: true},
                         {field: 'empNo', headerName: '사원 번호', width: 150},
                         {field: 'rawRegDate', headerName: '원자재 등록일', width: 150},
                         {field: 'rawRegUpdate', headerName: '원자재 수정일', width: 150},
